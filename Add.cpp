7075e97a9f630b1c7d751c2ca2f156916ea09718
Tausiq <tausiqsamantaray03@gmail.com>
2021-10-01 19:52:19 +0530

    Just modified add.cpp and added a .gitignore file I have modified add.cpp file. According to your request I have modified add.cpp file ->Added a factorial ->Added squareroot ->Added perfect square For .vscode file I have added a .gitignore file. Hope you like the modifications.

--- Add.cpp
+++ Add.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
@@ -15,10 +16,23 @@ int main()
 
   // calculate average value
   average = sum / 3;
+  // calculate factorial
+  long long int i=2,factorial=1;
+  while ((i<=num1)&&(i>=0))
+  {
+    factorial*=i;
+    i++;
+  }
 
   // display result
   cout << "Sum = " << sum << endl;
   cout << "Average = " << average << endl;
-
+  cout <<"Factorial of first no.="<<factorial<<endl;
+  if (num1>0)
+  cout <<"Square root of first no.="<<sqrt(num1)<<endl;
+  if (sqrt(num1)==int(sqrt(num1)))
+  cout<<num1<<"Its a perfect square.";
+  else
+  cout<<num1<<"Its not a perfect square.";
   return 0;
 }
